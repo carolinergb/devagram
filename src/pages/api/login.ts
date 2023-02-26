90e57c6dfa260a9541e0088391507ff0040be331
Caroline <carolineamrb@gmail.com>
2023-02-26 19:07:53 -0300

    alterações gerais

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,17 +1,21 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { conectaBD } from 'middlewares/conectaBD';
+import { conectarBD } from 'middlewares/conectarBD';
+import { UsuarioModel } from 'models/UsuarioModel';
+import md5 from 'md5';
 
-const login = (req: NextApiRequest, res: NextApiResponse) => {
+const endpointLogin = async (req: NextApiRequest, res: NextApiResponse) => {
   if(req.method === 'POST') {
     const { login, senha } = req.body;
 
-  if(login === 'usuario@usuario.com' && senha === 'usuario123') {
+    const usuarioEncontrado = await UsuarioModel.find({email: login, senha: md5(senha)})
+
+  if(usuarioEncontrado && usuarioEncontrado.length > 0) {
     res.status(200).json({msg: 'Usuário autenticado com sucesso'})
-  }
+  } 
   return res.status(400).json({erro: 'Usuário ou senha inválidos'})
   }
 
   return res.status(405).json({erro: 'Método informado não está correto'})
 }
 
-export default login;
+export default conectarBD(endpointLogin);
